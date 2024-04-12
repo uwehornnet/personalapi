7dd053ec7ce78c7a6ba9364e473b41e144312ed4
uhorn <uwehornnet@users.noreply.github.com>
2024-04-12 22:51:16 +0200

    next config for cors

--- middleware.js
+++ middleware.js
@@ -3,26 +3,25 @@ import { NextResponse } from "next/server";
 const jwt = `uhorn:${process.env.API_SECRET}`;
 
 export async function middleware(request) {
+	const res = NextResponse.next();
+
+	// add the CORS headers to the response
+	res.headers.append("Access-Control-Allow-Credentials", "true");
+	res.headers.append("Access-Control-Allow-Origin", "*"); // replace this your actual origin
+	res.headers.append("Access-Control-Allow-Methods", "GET,DELETE,PATCH,POST,PUT");
+	res.headers.append(
+		"Access-Control-Allow-Headers",
+		"X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
+	);
+
 	const auth = await request.headers.get("authorization");
 	const token = atob(auth.split(" ")[1]);
 
 	if (jwt !== token) {
 		return NextResponse.json({ success: false, message: "authentication failed" }, { status: 401 });
-	}else{
-		// retrieve the current response
-		const res = NextResponse.next()
-
-		// add the CORS headers to the response
-		res.headers.append('Access-Control-Allow-Credentials', "true")
-		res.headers.append('Access-Control-Allow-Origin', '*') // replace this your actual origin
-		res.headers.append('Access-Control-Allow-Methods', 'GET,DELETE,PATCH,POST,PUT')
-		res.headers.append(
-			'Access-Control-Allow-Headers',
-			'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-		)
-	
-		return res
 	}
+
+	return res;
 }
 
 export const config = {
